ebe3c50d228112c99ffe2e1f13b610bbe1fe645f
Joel Singh <joelsingh788@gmail.com>
2023-11-15 18:58:34 -0500

    Add sendWikipediaAPIRequest.ts

---
+++ wikiAPI/sendWikipediaAPIRequest.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from "axios";
+import { getAxiosVersion } from "./util/getAxiosVersion";
+
+export default function sendWikipediaAPIRequest(params: Object) {
+  const wikiAPIEndpoint = `https://en.wikipedia.org/w/api.php`;
+
+  const headers = {
+    "User-Agent": `WikiGiggles/0.0 (joelsingh788@gmail.org) axios/${getAxiosVersion()}`,
+  };
+
+  try {
+    return axios.get(wikiAPIEndpoint, { params, headers });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function getPageDataFromQueryResponse(response: AxiosResponse): Array<any> {
+  return response.data.query.pages;
+}
