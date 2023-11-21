1fe503ace939d5113ed22f485a6ad8af45e2bad4
Joel Singh <joelsingh788@gmail.com>
2023-11-21 14:10:01 -0500

    Initial vite + svelte + ts setup

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let immediateRevisions = "test";
+  const searchForImmediateRevisions = "";
+</script>
+
+<main>
+  <input class="page-to-search" type="text">
+  <button type="button">Search for immediate revisions</button>
+  <div id="immediate-revisions">{immediateRevisions}</div>
+</main>
+<style>
+</style>
