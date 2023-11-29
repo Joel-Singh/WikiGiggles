7a553c6b72e28f856b17ed660d4dc6a166847e1a
Joel Singh <joelsingh788@gmail.com>
2023-11-28 20:00:36 -0500

    Do more css

--- src/App.svelte
+++ src/App.svelte
@@ -10,15 +10,22 @@
 </script>
 
 <main class="h-full pt-3 flex flex-col items-center gap-2">
+  <h1 class="text-xl">
+    Welcome to <a href="./" class="text-sky-500 hover:underline text-3xl"
+      >WikiGiggles</a
+    >
+  </h1>
   <input
     bind:value={pageToSearch}
-    class="p-1 py-0.5 w-5/6 justify-self-center"
+    class="p-1 py-0.5 w-5/6 justify-self-center border-gray-400 border-solid border focus:border-sky-500 shadow-inner outline-0 transition-all"
     type="text"
+    placeholder="Enter the funny page"
   />
   <button
     on:click={searchForImmediateRevisions}
     type="button"
-    class="w-2/5 justify-self-center">Search for immediate revisions</button
+    class="w-2/5 justify-self-center text-sky-500 hover:underline"
+    >Search for immediate revisions</button
   >
 
   <div id="immediate-revisions">{immediateRevisions}</div>
