a65dbe9f74f429035cda31cbd0d4a828f44a7e0f
Joel Singh <joelsingh788@gmail.com>
2023-11-28 20:27:27 -0500

    Move funnySearcher into its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-  import getImmediateRevertEdits from "./wikiAPI/getImmediateRevertEdits";
-
-  let pageToSearch = "";
-  let immediateRevisions = "";
-  let searchForImmediateRevisions = async () => {
-    // @ts-ignore
-    immediateRevisions = await getImmediateRevertEdits(pageToSearch);
-  };
+  import FunnySearcher from "./components/funnySearcher.svelte";
 </script>
 
 <main class="h-full pt-3 flex flex-col items-center gap-2">
@@ -15,18 +8,6 @@
       >WikiGiggles</a
     >
   </h1>
-  <input
-    bind:value={pageToSearch}
-    class="p-1 py-0.5 w-5/6 justify-self-center border-gray-400 border-solid border focus:border-sky-500 shadow-inner outline-0 transition-all"
-    type="text"
-    placeholder="Enter the funny page"
-  />
-  <button
-    on:click={searchForImmediateRevisions}
-    type="button"
-    class="w-2/5 justify-self-center text-sky-500 hover:underline"
-    >Search for immediate revisions</button
-  >
 
-  <div id="immediate-revisions">{immediateRevisions}</div>
+  <FunnySearcher/>
 </main>
---
+++ src/components/funnySearcher.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import getImmediateRevertEdits from "../wikiAPI/getImmediateRevertEdits";
+
+  let pageToSearch = "";
+  let immediateRevisions = "";
+  let searchForImmediateRevisions = async () => {
+    // @ts-ignore
+    immediateRevisions = await getImmediateRevertEdits(pageToSearch);
+  };
+</script>
+
+<input
+  bind:value={pageToSearch}
+  class="p-1 py-0.5 w-5/6 justify-self-center border-gray-400 border-solid border focus:border-sky-500 shadow-inner outline-0 transition-all"
+  type="text"
+  placeholder="Enter the funny page"
+/>
+
+<button
+  on:click={searchForImmediateRevisions}
+  type="button"
+  class="w-2/5 justify-self-center text-sky-500 hover:underline"
+  >Search for immediate revisions</button
+>
+
+<div id="immediate-revisions">{immediateRevisions}</div>
