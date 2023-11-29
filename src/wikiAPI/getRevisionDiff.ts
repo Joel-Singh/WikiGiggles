042c9b9bd79336e213e22445bc6ca8ff1aefcfdd
Joel Singh <joelsingh788@gmail.com>
2023-11-28 21:25:51 -0500

    Add getRevisionDiff function

---
+++ src/wikiAPI/getRevisionDiff.ts
@@ -0,0 +1,15 @@
+import sendWikipediaAPIRequest from "./sendWikipediaAPIRequest";
+
+export default async function getRevisionDiff(revisionId: string) {
+  const params = {
+    action: "compare",
+    format: "json",
+    fromrev: revisionId,
+    torelative: "prev",
+    prop: "diff",
+    difftype: "inline",
+    formatversion: "2",
+  };
+  const response = await sendWikipediaAPIRequest(params);
+  return response.data.compare.body as string;
+}
