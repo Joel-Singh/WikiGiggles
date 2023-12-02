bd9aae173bdd24108d8a0e33b1b2d9c21236407e
Joel Singh <joelsingh788@gmail.com>
2023-12-02 18:59:12 -0500

    Move styles from app.css into App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   let immediateRevisions: number[] | null = null;
 </script>
 
-<main class="h-full pt-3 flex flex-col items-center gap-2">
+<main class="h-[100vh] overflow-hidden pt-3 flex flex-col items-center gap-2 bg-white text-zinc-900 [font-style: sans-serif]">
   <h1 class="text-xl">
     Welcome to <a href="./" class="text-sky-500 hover:underline text-3xl"
       >WikiGiggles</a
