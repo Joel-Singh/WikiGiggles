3af18db74b3ee1094639f8b8b96b21a14e22c74a
Joel Singh <joelsingh788@gmail.com>
2023-12-01 19:26:49 -0500

    Wrap funnyRevisionSearcher in a div

--- src/components/funnyRevisionSearcher.svelte
+++ src/components/funnyRevisionSearcher.svelte
@@ -8,16 +8,18 @@
   };
 </script>
 
-<input
-  bind:value={pageToSearch}
-  class="p-1 py-0.5 w-5/6 justify-self-center border-gray-400 border-solid border focus:border-sky-500 shadow-inner outline-0 transition-all"
-  type="text"
-  placeholder="Enter the funny page"
-/>
+<div class="flex flex-col items-center w-2/3">
+  <input
+    bind:value={pageToSearch}
+    class="p-1 py-0.5 w-5/6 justify-self-center border-gray-400 border-solid border focus:border-sky-500 shadow-inner outline-0 transition-all"
+    type="text"
+    placeholder="Enter the funny page"
+  />
 
-<button
-  on:click={searchForImmediateRevisions}
-  type="button"
-  class="w-2/5 justify-self-center text-sky-500 hover:underline"
-  >Search for immediate revisions</button
->
+  <button
+    on:click={searchForImmediateRevisions}
+    type="button"
+    class="w-2/5 justify-self-center text-sky-500 hover:underline"
+    >Search for immediate revisions</button
+  >
+</div>
